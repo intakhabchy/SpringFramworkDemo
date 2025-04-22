29269ee0ba921a67f04856aa80b743c939574703
intakhabchy <intakhab.chy@gmail.com>
2025-04-23 02:20:06 +0600

    annotation bean works

--- src/main/java/org/dailycodebuffer/Main.java
+++ src/main/java/org/dailycodebuffer/Main.java
@@ -2,23 +2,14 @@ package org.dailycodebuffer;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-//        Doctor doctor = context.getBean(Doctor.class);
-//        doctor.assist();
-
-//        Nurse nurse = context.getBean(Nurse.class);
-//        Nurse nurse = (Nurse) context.getBean("nurse");
-//        nurse.assist();
-
-//        Staff staff = context.getBean(Doctor.class);
-//        staff.assist();
-
         Doctor doctor = context.getBean(Doctor.class);
-        System.out.println(doctor.getQualification());
+        doctor.assist();
     }
 }
